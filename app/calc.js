d97b7611a4c47c2104ec486c0da62aa5ff61a6e4
[fsaraci] <festimesaraci1@gmail.com>
2022-06-14 11:00:28 +0200

    msg

--- app/calc.js
+++ app/calc.js
@@ -7,10 +7,6 @@ var num = [
     }
   ];
   
-  
-var result = num.reduce(function(_this, val) {
-    return _this + val.number
-}, 0);
 var num1 = [
     {
       "number": 4500
@@ -20,9 +16,15 @@ var num1 = [
     }
   ];
   
-  
-var resultt = num.reduce(function(_this, val) {
+  let array = Object.entries(num).map(([key,value])=>value);
+  let array1 = Object.entries(num1).map(([key,value])=>value);
+
+/*var result = num.reduce(function(_this, val) {
     return _this + val.number
 }, 0);
+var result1 = num1.reduce(function(_this, val) {
+    return _this + val.number
+}, 0);*/
 
-console.log(result+resultt)
+//console.log(result+result1)
+console.log(array[0].number+array1[1].number)
